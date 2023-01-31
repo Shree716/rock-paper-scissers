ae11ccb8797018d76aec470a214d1f7398acd2e9
Shree716 <122548030+Shree716@users.noreply.github.com>
2023-01-31 11:11:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    player_A += 1
+})
 function showScoreBoard () {
     OLED.clear()
     OLED.writeStringNewLine("player A'" + player_A)
@@ -8,15 +11,27 @@ function showScoreBoard () {
     OLED.newLine()
     OLED.writeStringNewLine("tie'" + tie)
 }
+input.onButtonPressed(Button.AB, function () {
+    tie += 1
+})
+input.onButtonPressed(Button.B, function () {
+    player_B += 1
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    player_A = 0
+    player_B = 0
+    rounds = 0
+    tie = 0
+    OLED.writeStringNewLine("Suiiiiiii!")
+    basic.pause(5000)
+    showScoreBoard()
+}
 let tie = 0
 let rounds = 0
 let player_B = 0
 let player_A = 0
-OLED.init(128, 64)
-player_A = 0
-player_B = 0
-rounds = 0
-tie = 0
-OLED.writeStringNewLine("Suiiiiiii!")
-basic.pause(5000)
-showScoreBoard()
+reset()
